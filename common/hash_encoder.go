31b28a1d86204d8d4d4448b60e695e5259a36096
Dustin Oprea <myselfasunder@gmail.com>
2017-01-12 09:56:37 -0500

    Added strong-only hash encoder.

--- common/hash_encoder.go
+++ common/hash_encoder.go
@@ -2,8 +2,10 @@ package ricommon
 
 import (
     "fmt"
+    "io"
 
     "encoding/gob"
+    "encoding/binary"
     "crypto/sha1"
 
     "github.com/dsoprea/go-logging"
@@ -18,6 +20,39 @@ func EncodeToSha1DigestString(parts []interface{}) (digest string) {
             log.Panic(fmt.Errorf("error encoding [%v]: %s", x, err))
         }
     }
+
+    digest = fmt.Sprintf("%x", h.Sum(nil))
+    return digest
+}
+
+func EncodeStringsToSha1DigestString(parts []string) (digest string) {
+    h := sha1.New()
+
+    for i, s := range parts {
+        if c, err := io.WriteString(h, s); err != nil {
+            log.Panic(fmt.Errorf("error writing [%s]: %s", s, err))
+        } else if c != len(s) {
+            log.Panic(fmt.Errorf("incorrect number of bytes written"))
+        }
+
+        // Push a NUL byte to separate between parts but not at the end. This 
+        // way, we can also use this function to hash single strings.
+        if i < len(parts) - 1 {
+            if c, err := h.Write([]byte { 0 }); err != nil {
+                log.Panic(err)
+            } else if c != 1 {
+                log.Panic(fmt.Errorf("incorrect number of bytes written"))
+            }
+        }
+    }
+
+    digest = fmt.Sprintf("%x", h.Sum(nil))
+    return digest
+}
+
+func EncodeInt64ToSha1DigestString(n int64) (digest string) {
+    h := sha1.New()
+    binary.Write(h, binary.LittleEndian, n)
 
     digest = fmt.Sprintf("%x", h.Sum(nil))
     return digest
